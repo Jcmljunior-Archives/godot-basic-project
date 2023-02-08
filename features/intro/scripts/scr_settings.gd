f1319a69aa59228c4c542e4fff56b2bbbc93750b
Julio Cesar <82642883+jcmljunior@users.noreply.github.com>
2023-02-08 00:20:36 -0300

    adicionado botão para retornar ao cenário inicial

--- features/intro/scripts/scr_settings.gd
+++ features/intro/scripts/scr_settings.gd
@@ -1,12 +1,15 @@
 extends Control
 
-onready var title = get_node("content/grid/title")
-onready var display_fps = get_node("content/grid/tab_container/Video/content/grid/vbox/display_fps")
-onready var master_volume = get_node("content/grid/tab_container/Audio/content/grid/vbox/label")
-onready var volume_min = get_node("content/grid/tab_container/Audio/content/grid/vbox/hbox/volume_min")
-onready var volume_max = get_node("content/grid/tab_container/Audio/content/grid/vbox/hbox/volume_max")
+onready var title: Label = get_node("content/grid/hbox/title")
+onready var display_fps: Label = get_node("content/grid/tab_container/Video/content/grid/vbox/display_fps")
+onready var check_display_fps: CheckButton = get_node("content/grid/tab_container/Video/content/grid/vbox/check")
+onready var master_volume: Label = get_node("content/grid/tab_container/Audio/content/grid/vbox/label")
+onready var volume_min: Label = get_node("content/grid/tab_container/Audio/content/grid/vbox/hbox/volume_min")
+onready var volume_max: Label = get_node("content/grid/tab_container/Audio/content/grid/vbox/hbox/volume_max")
 
 func _ready() -> void:
+	check_display_fps.pressed = Gui.display_fps
+	
 	update_locale()
 
 # INICIALIZAÇÃO DA TRADUÇÃO DE ELEMENTOS.
@@ -20,3 +23,6 @@ func update_locale() -> void:
 func _on_check_pressed() -> void:
 	Gui.update_display_fps()
 
+
+func _on_button_back_pressed() -> void:
+	get_tree().change_scene("res://features/intro/scenes/sce_start_game.tscn")
