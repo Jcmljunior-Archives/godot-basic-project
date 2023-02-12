218f9d9b3dcac96ee5bb2f42028065b10c4e4ce1
Julio Cesar <82642883+jcmljunior@users.noreply.github.com>
2023-02-11 23:13:31 -0300

    correções de código

--- setup.py
+++ setup.py
@@ -5,39 +5,43 @@ import shutil
 path = os.getcwd()
 words = []
 
-file_name = "global.pot"
-
-# Lista todos os arquivos com a extensão informada.
-def list_file_by_extension_in_path(path, extension):
-    file_path = []
+# Efetua uma busca por todos os arquivos contido em cada diretório relacionado a extensão informada.
+def list_files_in_directory_by_extension(extension):
+    list_files = []
     for root, _, files in os.walk(path):
-        for filename in files:
-            if not filename.endswith(extension) : continue
-            file_path.append(os.path.join(root, filename))
+       for file in files:
+            if file.endswith(extension):
+                list_files.append([
+                    root,
+                    file,
+                ])
 
-    return file_path
+    return list_files
 
+# Retorna o caminho base até as cenas.
+def get_path_for_scene(path):
+    return re.search('(?:features)\s*(.*$)', path).group(0)
 
-# Faz a leitura dos arquivos encontrado linha por linha e retorna o valor da chave texto através de regex.
-def read_file_by_extension_in_path(path, extension, keyword):
-    for file_path in list_file_by_extension_in_path(path, extension):
-        with open(file_path, "r", encoding="utf8") as file:
-            file_content = file.readlines()
-            
-            for line in file_content:
+# Retorna o valor da palavra chave encontrada.
+def get_param_value(line):
+    return re.search('"([^"]+)"', line).group(1)
+
+# Efetua a leitura dos arquivos relacionado a extensão encontrado.
+def read_files_in_directory_by_extension(extension, keyword):
+    for file in list_files_in_directory_by_extension(extension):
+        with open(os.path.join(get_path_for_scene(file[0]), file[1]), "r", encoding="utf8") as f:
+            lines = f.readlines()
+            f.close()
+
+            for line in lines:
                 if not keyword in line: continue
-                words.append(re.search('"([^"]+)"',line).group(1))
-    
-            file.close()
+                words.append(get_param_value(line))
+        break
 
-# Cria o arquivo de tradução caso não exista.
-def create_file_by_extension_in_path(file):
-    if not os.path.exists(file):
-        file = open(file, "x")
-        file.close()
 
-def generate_file(file):
-    file = open(file, "w")
+# Cria o arquivo caso não exista.
+def generate_file_in_path(file):
+    file = open(file, "a" if not os.path.exists(file) else "w", encoding="utf8")
 
     # Define o escopo inicial do arquivo.
     file.write("msgid " + '""')
@@ -45,6 +49,7 @@ def generate_file(file):
     file.write("msgstr " + '""')
     file.write("\n")
     file.write("\n")
+
 
     # Lista todas as palavras encontrada desde que não seja repetidas.
     for word in list(dict.fromkeys(words)):
@@ -54,23 +59,15 @@ def generate_file(file):
         file.write("\n")
         file.write("\n")
 
-    # Fecha a chamada do arquivo.
     file.close()
 
-def move_generated_file():
-    if(os.path.exists(file_name)):
-        if os.path.exists("/features/i18n/" + file_name):
-            os.remove(os.path.exists("/features/i18n/" + file_name))
+def move_generated_file(path):
+    if os.path.exists(path):
+        while os.path.exists(os.path.join("features", "i18n", path)):
+            os.remove(os.path.join("features", "i18n", path))
 
-        # Garante a exclusão do arquivo anterior caso exista.
-        while os.path.exists(path + "/features/i18n/" + file_name):
-            os.remove(path + "/features/i18n/" + file_name)
+        shutil.move(os.path.join(path), os.path.join("features", "i18n", path))
 
-        # Move o arquivo gerado para o diretório de idiomas.
-        shutil.move(path + "/" + file_name, path + "/features/i18n")
-
-
-read_file_by_extension_in_path(path, ".tscn", "text")
-create_file_by_extension_in_path(file_name)
-generate_file(file_name)
-move_generated_file()
+read_files_in_directory_by_extension(".tscn", "text")
+generate_file_in_path("global.pot")
+move_generated_file("global.pot")
